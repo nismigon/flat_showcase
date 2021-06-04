05f23ac8289a5cb195606c75bc90e4ba2db765e2
teissem <teissedre.mathieu@outlook.com>
2021-06-04 20:08:58 +0200

    Welcome screen
    Add :
    - Images in Carousel
    - Description
    - Status

--- index.php
+++ index.php
@@ -23,39 +23,122 @@
             </header>
         </div>
 
-        <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 20vw; height: 90vh">
-            <a href="index.php" class="d-flex align-items-center m-0 link-dark text-decoration-none justify-content-center flex-wrap">
-                <span class="fs-4">Liste des appartements</span>
-            </a>
-            <hr>
-            <ul class="nav nav-pills flex-column mb-auto ">
-                <?php
-                if (file_exists("configuration.json")) {
-                    $json = json_decode(file_get_contents("configuration.json"));
-                } else {
-                    $json = false;
-                }
-                if ($json === false) {
+        <div class="row" style="width: 100vw;">
+            <div class="col-3">
+                <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="min-height: 90vh; height : 100%">
+                    <a href="index.php" class="d-flex align-items-center m-0 link-dark text-decoration-none justify-content-center flex-wrap">
+                        <span class="fs-4">Liste des appartements</span>
+                    </a>
+                    <hr>
+                    <ul class="nav nav-pills flex-column mb-auto ">
+                        <?php
+                        if (file_exists("configuration.json")) {
+                            $json = json_decode(file_get_contents("configuration.json"));
+                        } else {
+                            $json = false;
+                        }
+                        if ($json === false) {
+                            ?>
+                            <li class="nav-item">
+                                <span class="nav-link" aria-current="page">
+                                    Aucun appartement disponible
+                                </span>
+                            </li>
+                            <?php
+                        }
+                        else {
+                            foreach($json as $key => $value) {
+                                ?>
+                                <li class="nav-item d-flex align-items-center justify-content-start">
+                                    <a href="index.php?id=<?php echo($key); ?>&image=0" class="nav-link <?php
+                                    if(isset($_GET["id"]) && $_GET["id"] == $key) {
+                                        echo("active");
+                                    }
+                                    ?>" style="width:100%">
+                                    <img src="dependencies/house.svg" class="me-2" width="20" height="20" />
+                                    <span><?php echo($value->{"titre_court"}) ?></span>
+                                    </a>
+                                </li>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-9">
+                <?php if(isset($_GET["id"])) {
                     ?>
-                    <li class="nav-item">
-                        <span class="nav-link" aria-current="page">
-                            Aucun appartement disponible
-                        </span>
-                    </li>
+                    <h1 class="text-center"><?php echo($json->{$_GET["id"]}->{"titre_long"});?></h1>
+                    <br />
+                    <hr style="width: 50%; margin: auto"/>
+                    <br />
+                    <div id="carouselImages" class="carousel slide" data-ride="carousel" style="max-width: 80%; margin : auto;">
+                        <div class="carousel-inner">
+                            <?php
+                                $count = 0;
+                                foreach ($json->{$_GET["id"]}->{"images"} as $image) {
+                                    ?>
+                                    <div class="carousel-item <?php if($count == $_GET["image"]) echo("active") ?>">
+                                        <img class="d-block w-100" src="<?php echo($image) ?>" alt="Image">
+                                    </div>
+                                    <?php
+                                    $count++;
+                                }
+                            ?>
+                        </div>
+                        <?php
+                        $next_image = ($_GET["image"] + 1) % count($json->{$_GET["id"]}->{"images"});
+                        $previous_image = ($_GET["image"] - 1) == -1 ? count($json->{$_GET["id"]}->{"images"}) -1 :
+                            ($_GET["image"] - 1) % count($json->{$_GET["id"]}->{"images"});
+                        $next_url = "index.php?id=" . $_GET["id"] . "&image=" . $next_image;
+                        $previous_url = "index.php?id=" . $_GET["id"] . "&image=" . $previous_image;
+                        ?>
+                        <a class="carousel-control-prev" href="<?php echo($previous_url) ?>" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="<?php echo($next_url) ?>" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <br />
+                    <hr style="width: 50%; margin: auto"/>
+                    <br />
                     <?php
-                }
-                else {
-                    foreach($json as $key => $value) {
+                        $description = str_replace("\n", "<br />", $json->{$_GET["id"]}->{"description"});
                     ?>
-                        <li class="nav-item mb-3 d-flex align-items-center justify-content-start">
-                            <img src="dependencies/house.svg" class="me-2" width="20" height="20" />
-                            <span><?php echo($value->{"titre_court"}) ?></span>
+                    <ul class="list-group" style="width: 80%; margin: auto">
+                        <li class="list-group-item active"><h2>Description</h2></li>
+                        <li class="list-group-item"><?php echo($description); ?></li>
+                    </ul>
+                    <br />
+                    <hr style="width: 50%; margin: auto"/>
+                    <br />
+                    <?php
+                        if ($json->{$_GET["id"]}->{"status_color"} == "green") {
+                            $status_color = "list-group-item-success";
+                        } else if ($json->{$_GET["id"]}->{"status_color"} == "red") {
+                            $status_color = "list-group-item-danger";
+                        } else if ($json->{$_GET["id"]}->{"status_color"} == "yellow") {
+                            $status_color = "list-group-item-warning";
+                        } else {
+                            $status_color = "list-group-item-light";
+                        }
+                    ?>
+                    <ul class="list-group" style="width: 80%; margin: auto">
+                        <li class="list-group-item active">
+                            <h2>Status</h2>
                         </li>
+                        <li class="list-group-item <?php echo($status_color)?>">
+                            <?php echo($json->{$_GET["id"]}->{"status_text"}); ?>
+                        </li>
+                    </ul>
+                    <br />
                     <?php
-                    }
-                }
-                ?>
-            </ul>
+                } ?>
+            </div>
         </div>
     </div>
 </body>
